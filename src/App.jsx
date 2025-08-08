1cdd821283d6ad041e9151e0456a4cb8c760c07e
rafael <calixxtorafaell@gmail.com>
2025-08-08 11:46:50 -0300

    fix: fix to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import useDarkMode from 'use-dark-mode';
 import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
@@ -11,11 +10,10 @@ import { lightTheme, darkTheme } from './theme/themes';
 
 function App() {
   window.matchMedia = null;
-  const darkMode = useDarkMode(true);
 
   return (
-    <AppContext.Provider value={{ darkMode }}>
-      <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
+    <AppContext.Provider value={{ }}>
+      <ThemeProvider theme={lightTheme}>
         <GlobalStyles />
         <div className="App">
           <BrowserRouter>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import { Container } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
@@ -51,17 +50,9 @@ function Experience(props) {
         ? (
           <div className="section-content-container">
             <Container>
-              <Timeline
-                lineColor={theme.timelineLineColor}
-              >
+              <div>
                 {data.map((item) => (
-                  <TimelineItem
-                    key={item.title + item.dateText}
-                    dateText={item.dateText}
-                    dateInnerStyle={{ background: theme.accentColor }}
-                    style={styles.itemStyle}
-                    bodyContainerStyle={{ color: theme.color }}
-                  >
+                  <div>
                     <h2 className="item-title">
                       {item.title}
                     </h2>
@@ -92,9 +83,9 @@ function Experience(props) {
                         </div>
                       ))}
                     </ul>
-                  </TimelineItem>
+                  </div>
                 ))}
-              </Timeline>
+              </div>
             </Container>
           </div>
         ) : <FallbackSpinner /> }
